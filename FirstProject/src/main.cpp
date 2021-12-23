6786e3fab32f29e7b0ee6bdf5196d7628e0a22a7
Tiffany <tiffanychien9@gmail.com>
2021-12-23 18:26:33 -0500

    init

---
+++ FirstProject/src/main.cpp
@@ -0,0 +1,18 @@
+//  Ask the user for number and display
+// Need input output libraries
+#include <iostream>
+
+// compile -> link -> create exe
+
+int main()
+{
+  int favorite_number;
+  // Statement must end with semicolin
+  std::cout << "Enter your favorite number between 1 and 100: ";
+  // cin reads in the concole
+  std::cin >> favorite_number;
+  // std:endl endline in the end to flush the buffer
+  std::cout << "Amazing! That's my favorite number too!" << std::endl;
+  // Main returns an integer, return 0 if good
+  return 0;
+}
