6951f3a5faa8f5b992261f13711e821a3e18d0f5
Tiffany <tiffanychien9@gmail.com>
2021-12-25 03:49:21 -0500

    section05

--- Challenge/src/main.cpp
+++ Challenge/src/main.cpp
@@ -1,13 +1,21 @@
+// preprocessor: start with # - getting the source code ready for the compiler
 #include <iostream>
 
+// Using the entire std namespace
+using namespace std;
+// Can specify name space
+using std::cout;
+
+// main should return 0 to indicate the program successfully executed
+// main, std, cin, cout, endl: identifier
 int main()
 {
   int favorite_number;
-
-  std::cout << "Please choose your favorite number from 1 to 100";
-  std::cin >> favorite_number;
-  std::cout << "That's my favorite number too!" << std::endl;
+  // std - namespaces
+  cout << "Please choose your favorite number from 1 to 100";
+  cin >> favorite_number;
+  cout << "That's my favorite number too!" << endl;
   // We want to add on favorite number
-  std::cout << "No really!!, " << favorite_number << " is my favorite number" << std::endl;
+  cout << "No really!!, " << favorite_number << " is my favorite number" << endl;
   return 0;
 }
