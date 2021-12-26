6e8818e633be6e1ec6f6984f30bc2b2fe0d0f93b
Tiffany <tiffanychien9@gmail.com>
2021-12-26 05:34:17 -0500

    section05 with double

--- section05/src/basic.cpp
+++ section05/src/basic.cpp
@@ -4,4 +4,30 @@ using namespace std;
 
 int main()
 {
+  // HelloWorld!
+  cout << "Hello";
+  cout << "World!" << endl;
+  cout << "Hello"
+       << " World!" << endl;
+  cout << "Hello World!\n"
+       << endl;
+
+  // can insert by {int} space {int}
+  int num1;
+  int num2;
+  cout << "Enter 2 integers separated with a space";
+  cin >> num1 >> num2;
+  cout << "You entered" << num1 << " and " << num2 << endl;
+
+  // If user only input 10.5
+  // we will get num1 = 10 and double = 0.5
+  doube doube1;
+  cout << "Enter an integer";
+  cin >> num1;
+  cout << "Enter a double";
+  cin >> double1;
+  cout << "The integer is " << num1 << endl;
+  cout << "The double is " << double1 << endl;
+
+  return 0;
 }
