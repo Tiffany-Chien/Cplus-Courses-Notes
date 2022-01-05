4a87ae43fe7fdb89c005e8b2a3faa41534eaa5e2
Tiffany <tiffanychien9@gmail.com>
2022-01-05 15:59:22 -0500

    opertators

---
+++ section06-operators/increment-decrement/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int result{0};
+    int counter{20};
+
+    // ++ prefix performs before the counter operation
+    // preincrement: increment before using
+    result = ++counter + 10;
+    // equivalent to:
+    // counter = counter + 1;
+    // result = counter + 10;
+    // result is 21;
+
+    // ++ postfix performs after the counter is operated
+    result = counter++ + 10;
+    // equivalent to:
+    // result = counter + 10;
+    // counter += 1;
+    // result is 20
+}
